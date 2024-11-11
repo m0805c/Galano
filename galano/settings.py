6ce26871926c1b161f961a84c429229c27524f26
Marcelo Chavez <marcelo.chavezcvr@gmail.com>
2024-11-11 02:09:01 -0600

    lib pymysql para la db/MySQL / Config de la db

--- galano/settings.py
+++ galano/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import pymysql # type: ignore
+pymysql.install_as_MySQLdb()
 
 from pathlib import Path
 
@@ -75,8 +77,13 @@ WSGI_APPLICATION = 'galano.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'galanodb',
+        'USER': 'root',
+        'PASSWORD':'A314641z',
+        'HOST':'localhost',
+        'PORT':'3306',
+
     }
 }
 
